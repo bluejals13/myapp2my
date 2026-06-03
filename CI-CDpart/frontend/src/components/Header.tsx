30302998efeb457ffcd565f9749bc631cf6e4d48
bluejals13 <yoriya10113@gmail.com>
2026-06-03 16:24:56 +0900

    Update Header.tsx

--- CI-CDpart/frontend/src/components/Header.tsx
+++ CI-CDpart/frontend/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../components/AuthContext";
+import { useAuth } from "../auth/AuthContext";
 
 export default function Header() {
   const navigate = useNavigate();
