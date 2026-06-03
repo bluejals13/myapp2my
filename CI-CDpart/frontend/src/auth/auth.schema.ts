d2142593345a81ef61e3b5f4cf10a07bc3dfdf53
bluejals13 <yoriya10113@gmail.com>
2026-06-03 17:17:37 +0900

    Update auth.schema.ts

--- CI-CDpart/frontend/src/auth/auth.schema.ts
+++ CI-CDpart/frontend/src/auth/auth.schema.ts
@@ -2,20 +2,14 @@
 
 import { z } from "zod";
 
-// 1. request schema (로그인 입력값)
+// 로그인 / 회원가입 공통 input
 export const authBaseSchema = z.object({
-  username: z
-    .string()
-    .trim()
-    .min(2, "아이디는 2자 이상"),
-
-  password: z
-    .string()
-    .trim()
-    .min(3, "비밀번호는 3자 이상"),
+  username: z.string().trim().min(2, "아이디는 2자 이상"),
+  password: z.string().trim().min(3, "비밀번호는 3자 이상"),
 });
 
-
-export const loginSchema = authBaseSchema; // 구조 개선 로그인 인풋 > 로그인 스키마
+export const loginSchema = authBaseSchema;
+export const signupSchema = authBaseSchema;
 
 export type LoginInput = z.infer<typeof loginSchema>;
+export type SignupInput = z.infer<typeof signupSchema>;
