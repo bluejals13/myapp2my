28c074cd0850299983fd037c4ffae347b5f4f5d1
bluejals13 <yoriya10113@gmail.com>
2026-06-03 22:27:51 +0900

    Delete CI-CDpart/frontend/src/utils directory

--- CI-CDpart/frontend/src/utils/validateAuth.ts
+++
@@ -1,9 +0,0 @@
-// auth.schema.ts
-import { z } from "zod";
-
-export const authBaseSchema = z.object({
-  username: z.string().min(2, "아이디는 2자 이상"),
-  password: z.string().min(3, "비밀번호는 3자 이상"),
-});
-
-export type LoginInput = z.infer<typeof authBaseSchema>;
