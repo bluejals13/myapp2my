d6a5054f9514fdca4726c77b6d2c66e94f155265
bluejals13 <yoriya10113@gmail.com>
2026-06-03 17:25:12 +0900

    Update Signup.tsx

--- CI-CDpart/frontend/src/pages/Signup.tsx
+++ CI-CDpart/frontend/src/pages/Signup.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 import { apiFetch } from "../api";
 import { signupSchema } from "../auth/auth.schema";
