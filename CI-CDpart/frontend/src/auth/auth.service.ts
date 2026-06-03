209d32a5d85777dd421dcf618411af16857bdaff
bluejals13 <yoriya10113@gmail.com>
2026-06-03 19:18:28 +0900

    Update auth.service.ts

--- CI-CDpart/frontend/src/auth/auth.service.ts
+++ CI-CDpart/frontend/src/auth/auth.service.ts
@@ -1,17 +1,13 @@
 const TOKEN_KEY = "token";
-const USER_KEY = "username";
 
 export const authStorage = {
   getToken: () => localStorage.getItem(TOKEN_KEY),
-  getUsername: () => localStorage.getItem(USER_KEY),
 
-  set: (token: string, username: string) => {
+  set: (token: string) => {
     localStorage.setItem(TOKEN_KEY, token);
-    localStorage.setItem(USER_KEY, username);
   },
 
   clear: () => {
     localStorage.removeItem(TOKEN_KEY);
-    localStorage.removeItem(USER_KEY);
   },
 };
