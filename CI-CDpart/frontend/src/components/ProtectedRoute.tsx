ef15a3317d389c544771dc07dcb51fb44bba9494
bluejals13 <yoriya10113@gmail.com>
2026-06-03 16:24:43 +0900

    Update ProtectedRoute.tsx

--- CI-CDpart/frontend/src/components/ProtectedRoute.tsx
+++ CI-CDpart/frontend/src/components/ProtectedRoute.tsx
@@ -1,15 +1,13 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { useAuth } from "./AuthContext";
+import { useAuth } from "../auth/AuthContext";
 
 export default function ProtectedRoute() {
-  const { token } = useAuth();
+  const { isLoggedIn, loading } = useAuth();
   const location = useLocation();
 
-  // 🔥 핵심: 아직 auth 판단 중이면 아무것도 안 그림
- // if (loading) return null;
+  if (loading) return null;
 
-  // 로그인 안 되어 있으면 로그인 페이지로
-  if (!token) {
+  if (!isLoggedIn) {
     return (
       <Navigate
         to="/login"
