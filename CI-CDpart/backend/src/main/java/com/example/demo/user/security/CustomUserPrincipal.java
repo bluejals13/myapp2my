2557df6da742885bd7f9742c2c12e3634f3451a7
bluejals13 <yoriya10113@gmail.com>
2026-06-05 00:50:52 +0900

    Update CustomUserPrincipal.java

--- CI-CDpart/backend/src/main/java/com/example/demo/user/security/CustomUserPrincipal.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/user/security/CustomUserPrincipal.java
@@ -10,12 +10,5 @@ public class CustomUserPrincipal {
     private Long userId;
     private final String username;
 
-    public CustomUserPrincipal(Long userId, String username) {
-        this.userId = userId;
-        this.username = username;
-    }
-
-    public Long getUserId() { return userId; }
-    public String getUsername() { return username; }
 }
 
