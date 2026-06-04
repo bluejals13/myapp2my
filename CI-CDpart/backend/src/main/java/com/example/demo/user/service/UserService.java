b1faac1a3a7ea603e8348c916d897a7cfd449f61
bluejals13 <yoriya10113@gmail.com>
2026-06-04 14:53:29 +0900

    Update UserService.java

--- CI-CDpart/backend/src/main/java/com/example/demo/user/service/UserService.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/user/service/UserService.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import java.time.Duration; // 시간
 
