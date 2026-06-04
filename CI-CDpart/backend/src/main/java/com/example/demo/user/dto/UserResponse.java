09825ef8c52cb34208662409bd516e4e011c72f0
bluejals13 <yoriya10113@gmail.com>
2026-06-05 00:56:18 +0900

    Update UserResponse.java

--- CI-CDpart/backend/src/main/java/com/example/demo/user/dto/UserResponse.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/user/dto/UserResponse.java
@@ -3,4 +3,9 @@ package com.example.demo.user.dto;
 public record UserResponse(
         Long id,
         String username
-) {}
+) {
+        return new UserResponse(
+        user.getId(),
+        user.getUsername()
+);
+}
