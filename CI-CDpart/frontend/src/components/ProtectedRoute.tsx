9add1ba69839c664ed80a65c0a8ada23134aac58
bluejals13 <yoriya10113@gmail.com>
2026-06-04 22:56:49 +0900

    Update ProtectedRoute.tsx

--- CI-CDpart/frontend/src/components/ProtectedRoute.tsx
+++ CI-CDpart/frontend/src/components/ProtectedRoute.tsx
@@ -2,17 +2,14 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../auth/AuthContext";
 
 export default function ProtectedRoute() {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, isLoading } = useAuth();
   const location = useLocation();
-
+  const from = location.state?.from?.pathname || "/main";
+  
+  if (isLoading) return null; // or spinner 으로 돌게
+  
   if (!isLoggedIn) {
-    return (
-      <Navigate
-        to="/login"
-        replace
-        state={{ from: location }}
-      />
-    );
+    return <Navigate to="/login" replace state={{ from: location }} />;
   }
 
   return <Outlet />;
