b9818f6050a6613300c5545fc0ddad4514fbba98
bluejals13 <yoriya10113@gmail.com>
2026-06-04 12:18:56 +0900

    12:18
    살짝 수정

--- CI-CDpart/backend/src/main/java/com/example/demo/user/security/AuthService.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/user/security/AuthService.java
@@ -13,18 +13,23 @@ public class AuthService {
 
     public TokenResponse refresh(String refreshToken) {
 
+        // 1. 토큰에서 userId 추출
         Long userId = jwtProvider.getUserId(refreshToken);
 
+        // 2. Redis에서 저장된 refreshToken 조회
         String saved = redisTemplate.opsForValue()
                 .get("refresh:" + userId);
 
+        // 3. 검증
         if (saved == null || !refreshToken.equals(saved)) {
             throw new BadCredentialsException("INVALID_REFRESH_TOKEN");
         }
 
+        // 4. 유저 조회
         User user = userRepository.findById(userId)
                 .orElseThrow();
 
+        // 5. 새로운 Access Token 발급
         String newAccessToken =
                 jwtProvider.createToken(user.getId(), user.getUsername());
 
