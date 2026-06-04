7cce5483fbc9393b0121631588896aa8dc831317
bluejals13 <yoriya10113@gmail.com>
2026-06-04 14:20:48 +0900

    Update AuthService.java

--- CI-CDpart/backend/src/main/java/com/example/demo/user/security/AuthService.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/user/security/AuthService.java
@@ -1,7 +1,13 @@
 package com.example.demo.user.security;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.user.domain.User;
+import com.example.demo.user.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
