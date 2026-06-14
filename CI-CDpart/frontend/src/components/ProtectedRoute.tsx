c0ba50adab7d0a50686bb23bd412e312ca47766d
bluejals13 <yoriya10113@gmail.com>
2026-06-14 14:44:16 +0900

    Update ProtectedRoute.tsx

--- CI-CDpart/frontend/src/components/ProtectedRoute.tsx
+++ CI-CDpart/frontend/src/components/ProtectedRoute.tsx
@@ -1,4 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
+import FullPageSpinner from "../components/FullPageSpinner";
 import { useAuth } from "../auth/AuthContext";
 
 export default function ProtectedRoute() {	// 깜박거리는 거 로딩화면으로 수정
