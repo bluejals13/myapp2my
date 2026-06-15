8dcf3318e64eb342857145967f306ea230516883
bluejals13 <yoriya10113@gmail.com>
2026-06-15 15:55:37 +0900

    Update RoleAdminService.java

--- CI-CDpart/backend/src/main/java/com/example/demo/admin/role/service/RoleAdminService.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/role/service/RoleAdminService.java
@@ -9,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.stream.Collectors;
 import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
