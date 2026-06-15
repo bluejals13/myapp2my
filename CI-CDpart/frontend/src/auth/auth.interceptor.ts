9aa7448f922174c7e89ef2985ccde3874d502c2e
bluejals13 <yoriya10113@gmail.com>
2026-06-15 21:24:53 +0900

    Update auth.interceptor.ts

--- CI-CDpart/frontend/src/auth/auth.interceptor.ts
+++ CI-CDpart/frontend/src/auth/auth.interceptor.ts
@@ -3,31 +3,46 @@ import { apiFetch } from "../api";
 import { authStorage } from "./auth.storage";	// jwt 토큰 키 get, set, clear
 import { refreshToken } from "./auth.manager";
 
+let refreshPromise: Promise<string | null> | null = null;
+
 export async function apiWithAuth<T>(
   url: string,
   options: RequestInit = {}
 ): Promise<T> {
   try {
     return await apiFetch<T>(url, options);
-  } catch (err: any) {
-    const status = err?.status;
+  } catch (err: any) { 
+    
+    const status = err?.status;                  
+    if (status !== 401) { throw err; }                  
+    
+    const original = options as any;
+    if (original._retry) throw err;
+      original._retry = true;
+                      
+    try {
+      if (!refreshPromise) {
+        refreshPromise = refreshToken().finally(() => {
+          isRefreshing = false;
+          refreshPromise = null;
+        });
+      }
 
-    if (status !== 401) { throw err; }
-
-    const newToken = await refreshToken();
-
+    const newToken = await refreshPromise;
+      
     if (!newToken) { throw err; }
 
     authStorage.set(newToken);
 
-    // retry 1번만 
     const headers = new Headers(options.headers ?? {});
-
     headers.set("Authorization", `Bearer ${newToken}`);
 
     return apiFetch<T>(url, {
       ...options,
       headers,
     });
+  } catch (e) {
+      throw e;
+    }   
   }
 }
