d21079433ffae3512d940b0164e77ab761d1adeb
bluejals13 <yoriya10113@gmail.com>
2026-06-16 00:40:15 +0900

    Update UserAdminService.java

--- CI-CDpart/backend/src/main/java/com/example/demo/admin/user/service/UserAdminService.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/user/service/UserAdminService.java
@@ -1,6 +1,6 @@
 package com.example.demo.admin.user.service;
 
-import com.example.demo.admin.user.dto.UserResponse;
+import com.example.demo.admin.user.dto.AdminUserResponse;
 import com.example.demo.user.domain.UserStatus;
 
 import com.example.demo.user.repository.UserRepository;
