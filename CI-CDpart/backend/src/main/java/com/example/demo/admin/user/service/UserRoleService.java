5fce6f52cb9409e0ad3d956d422b02691d0f7724
bluejals13 <yoriya10113@gmail.com>
2026-06-16 08:42:54 +0900

    Add files via upload

---
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/user/service/UserRoleService.java
@@ -0,0 +1,41 @@
+package com.example.demo.admin.user.service;
+
+import com.example.demo.user.domain.User;
+import com.example.demo.user.repository.UserRepository;
+import com.example.demo.admin.role.domain.Role;
+import com.example.demo.admin.role.repository.RoleRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserRoleService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public void assignRoles(
+            Long userId,
+            List<Long> roleIds
+    ) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow();
+
+        Set<Role> roles =
+                new HashSet<>(
+                        roleRepository.findAllById(roleIds)
+                );
+
+        user.setRoles(roles);
+    }
+}
