a2c575cc92adc7bcffc5a58da7bb5226e9b58b52
bluejals13 <yoriya10113@gmail.com>
2026-06-15 16:58:20 +0900

    Update PermissionPage.tsx

--- CI-CDpart/frontend/src/pages/Fpermi/PermissionPage.tsx
+++ CI-CDpart/frontend/src/pages/Fpermi/PermissionPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { apiFetch } from "../../api";
-import { useAuth } from "../../auth/AuthContext";
+import { apiFetch } from "@/api";
+import { useAuth } from "@/auth/AuthContext";
 import styles from "./permission.module.css";
 
 type Permission = {
