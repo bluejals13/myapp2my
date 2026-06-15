8e833203bf9cc5ea0f0604d8f6661bffbf72e637
bluejals13 <yoriya10113@gmail.com>
2026-06-15 15:41:41 +0900

    Update AuditRepository.java

--- CI-CDpart/backend/src/main/java/com/example/demo/admin/audit/repository/AuditRepository.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/audit/repository/AuditRepository.java
@@ -1,5 +1,8 @@
 package com.example.demo.admin.audit.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 public interface AuditRepository extends JpaRepository<Audit, Long> {
 
     List<Audit> findByUserId(Long userId);
