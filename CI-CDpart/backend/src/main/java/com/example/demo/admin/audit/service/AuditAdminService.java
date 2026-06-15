dad70d921ecf6f118f7bafc7107fcec4ba6a7be5
bluejals13 <yoriya10113@gmail.com>
2026-06-15 16:25:26 +0900

    Update AuditAdminService.java

--- CI-CDpart/backend/src/main/java/com/example/demo/admin/audit/service/AuditAdminService.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/audit/service/AuditAdminService.java
@@ -29,7 +29,7 @@ public class AuditAdminService {
         }
 
         return audits.stream()
-                .map(RoleResponse::from)
+                .map(AuditResponse::from)
                 .toList();
     }
 
