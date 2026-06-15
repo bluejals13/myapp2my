3921a178533dc5a41935652c900172490fdae978
bluejals13 <yoriya10113@gmail.com>
2026-06-15 17:15:30 +0900

    Update MenuPage.tsx

--- CI-CDpart/frontend/src/pages/Fmenu/MenuPage.tsx
+++ CI-CDpart/frontend/src/pages/Fmenu/MenuPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { apiFetch } from "@/api";
-import { useAuth } from "@/auth/AuthContext";
+import { apiFetch } from "@/api.ts";
+import { useAuth } from "@/auth/AuthContext.tsx";
 import styles from "./menu.module.css";
 
 type Menu = {
