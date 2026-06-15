6aa1f3172a9182a8ceae1fa86a0e3e880441092e
bluejals13 <yoriya10113@gmail.com>
2026-06-15 16:05:36 +0900

    Update AuditResponse.java

--- CI-CDpart/backend/src/main/java/com/example/demo/admin/audit/dto/AuditResponse.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/audit/dto/AuditResponse.java
@@ -1,13 +1,26 @@
 package com.example.demo.admin.audit.dto;
 
+import com.example.demo.admin.audit.domain.Audit;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
 public class AuditResponse {
+
     private Long id;
     private Long userId;
     private String action;
-    private String createdAt;
+    private LocalDateTime createdAt;
+
+    public static AuditResponse from(Audit audit) {
+        return new AuditResponse(
+                audit.getId(),
+                audit.getUserId(),
+                audit.getAction(),
+                audit.getCreatedAt()
+        );
+    }
 }
