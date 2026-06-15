ca22e934bab1ddb15fde9ccc4426ba52a6a9aee4
bluejals13 <yoriya10113@gmail.com>
2026-06-15 18:04:33 +0900

    Update EmptyLayout.tsx

--- CI-CDpart/frontend/src/layout/EmptyLayout.tsx
+++ CI-CDpart/frontend/src/layout/EmptyLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet, Link } from 'react-router-dom'
 
 export default function EmptyLayout() {
   return (
+    <>
       <header style={{ padding: 12, borderBottom: "1px solid #ddd" }}>
         <nav style={{ display: "flex", gap: 12 }}>
           <Link to="/">Main</Link>
@@ -9,8 +10,10 @@ export default function EmptyLayout() {
           <Link to="/signup">Sign up</Link>
         </nav>
       </header>
-    <main style={{ padding: 20 }}>
-      <Outlet />
-    </main>
+
+      <main style={{ padding: 20 }}>
+        <Outlet />
+      </main>
+    </>
   )
 }
