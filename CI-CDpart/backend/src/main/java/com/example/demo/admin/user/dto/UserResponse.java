95cb46f2736f83338d26080af5ee83b390dbe913
bluejals13 <yoriya10113@gmail.com>
2026-06-16 00:38:15 +0900

    Update UserResponse.java

--- CI-CDpart/backend/src/main/java/com/example/demo/admin/user/dto/UserResponse.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/user/dto/UserResponse.java
@@ -1,5 +1,7 @@
 package com.example.demo.admin.user.dto;
 
+import com.example.demo.user.domain.UserStatus;
+
 import lombok.Getter;
 import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
