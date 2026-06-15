1c01905f77d3a6ae79712b5839097e3b970880dd
bluejals13 <yoriya10113@gmail.com>
2026-06-15 16:22:29 +0900

    Update UserResponse.java

--- CI-CDpart/backend/src/main/java/com/example/demo/admin/user/dto/UserResponse.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/user/dto/UserResponse.java
@@ -2,8 +2,11 @@ package com.example.demo.admin.user.dto;
 
 import java.time.LocalDateTime;
 
+@Getter
+@AllArgsConstructor
 public class UserResponse {
     private Long id;
     private String username;
+    private String status;
     private LocalDateTime passwordChangedAt;
 }
