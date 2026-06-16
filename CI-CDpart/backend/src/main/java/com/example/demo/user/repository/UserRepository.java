a711f6c76302c7d7adaf5aecfca4e706834a9c31
bluejals13 <yoriya10113@gmail.com>
2026-06-16 10:19:59 +0900

    Update UserRepository.java

--- CI-CDpart/backend/src/main/java/com/example/demo/user/repository/UserRepository.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/user/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.demo.user.domain.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
