4bac8014ee35517decd6d896beb7c6cdd2d844be
bluejals13 <yoriya10113@gmail.com>
2026-06-16 10:27:34 +0900

    Update JpaConfig.java

--- CI-CDpart/backend/src/main/java/com/example/demo/admin/config/JpaConfig.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.config;
+package com.example.demo.admin.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
