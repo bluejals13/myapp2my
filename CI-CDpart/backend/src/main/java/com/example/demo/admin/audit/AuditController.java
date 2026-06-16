c837c031c13457b0aa8aad9e6c02d5ed8b9bf090
bluejals13 <yoriya10113@gmail.com>
2026-06-16 11:51:07 +0900

    Update AuditController.java

--- CI-CDpart/backend/src/main/java/com/example/demo/admin/audit/AuditController.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/audit/AuditController.java
@@ -1,7 +1,7 @@
 package com.example.demo.admin.audit;
 
 import com.example.demo.admin.audit.dto.AuditResponse;
-import com.example.demo.admin.audit.service.AuditAdminService;
+import com.example.demo.admin.audit.service.AuditService;
 import com.example.demo.admin.audit.service.AuditService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,9 +17,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/audits")
 @RequiredArgsConstructor
-public class AuditAdminController {
+public class AuditController {
 
-    private final AuditAdminService auditAdminService;
+    private final AuditService auditService;
 
     @GetMapping
     public List<AuditResponse> getAudits(
