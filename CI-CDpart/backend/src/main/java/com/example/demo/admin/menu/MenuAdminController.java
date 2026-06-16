aec39a596eb88f2b367f07a1c92165e2436d95fd
bluejals13 <yoriya10113@gmail.com>
2026-06-16 23:14:32 +0900

    Update MenuAdminController.java

--- CI-CDpart/backend/src/main/java/com/example/demo/admin/menu/MenuAdminController.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/menu/MenuAdminController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin/menus")
+@RequestMapping("/api/admin/menus")
 @RequiredArgsConstructor
 public class MenuAdminController {
 
