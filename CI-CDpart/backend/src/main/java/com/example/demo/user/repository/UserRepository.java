823b6d02bf66ae26647125ba084b6f7c43d10c95
bluejals13 <yoriya10113@gmail.com>
2026-06-16 10:49:01 +0900

    Update UserRepository.java

--- CI-CDpart/backend/src/main/java/com/example/demo/user/repository/UserRepository.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/user/repository/UserRepository.java
@@ -13,6 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
         "roles.permissions"
     })
     Optional<User> findUserWithRolesById(Long id);
+    Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username);
 }
