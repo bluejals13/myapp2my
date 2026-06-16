688bbf896ef5c57193878729e3c144ddfc8ec9f2
bluejals13 <yoriya10113@gmail.com>
2026-06-16 11:45:28 +0900

    Update SecurityUtil.java

--- CI-CDpart/backend/src/main/java/com/example/demo/admin/config/SecurityUtil.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/config/SecurityUtil.java
@@ -11,10 +11,14 @@ import com.example.demo.user.security.CustomUserPrincipal;
 public class SecurityUtil {
 
     public Long getUserId() {
-        return ((CustomUserPrincipal)
-                SecurityContextHolder.getContext()
-                        .getAuthentication()
-                        .getPrincipal()
-        ).getUserId();
+        Object principal = SecurityContextHolder.getContext()
+        .getAuthentication()
+        .getPrincipal();
+
+        if (principal instanceof CustomUserPrincipal p) {
+            return p.getUserId();
+        }
+
+        throw new IllegalStateException("Invalid authentication");
     }
 }
