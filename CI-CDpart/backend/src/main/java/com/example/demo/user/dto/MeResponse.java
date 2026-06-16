5580e519ade3981e72786e1aa07621593e128239
bluejals13 <yoriya10113@gmail.com>
2026-06-16 22:49:58 +0900

    Update MeResponse.java

--- CI-CDpart/backend/src/main/java/com/example/demo/user/dto/MeResponse.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/user/dto/MeResponse.java
@@ -1,5 +1,8 @@
 package com.example.demo.user.dto;
 
+import com.example.demo.admin.role.domain.Role;
+import com.example.demo.admin.permission.domain.Permission;
+
 import java.util.List;
 
 public record MeResponse(
