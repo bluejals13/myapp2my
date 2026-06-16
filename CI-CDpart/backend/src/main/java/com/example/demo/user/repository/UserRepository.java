8155ef44d804d515209f39d5c11bff4ae9d47090
bluejals13 <yoriya10113@gmail.com>
2026-06-16 12:08:00 +0900

    Update UserRepository.java

--- CI-CDpart/backend/src/main/java/com/example/demo/user/repository/UserRepository.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/user/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.user.repository;
 
 import com.example.demo.user.domain.User;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.EntityGraph;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -13,6 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
         "roles.permissions"
     })
     Optional<User> findWithRolesById(Long id);
-    
+
     boolean existsByUsername(String username);
 }
