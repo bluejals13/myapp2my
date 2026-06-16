9396567ef1c24633e34c2be7e5541ac8568dbc82
bluejals13 <yoriya10113@gmail.com>
2026-06-16 11:16:29 +0900

    Update AuditAction.java

--- CI-CDpart/backend/src/main/java/com/example/demo/admin/audit/domain/AuditAction.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/audit/domain/AuditAction.java
@@ -2,15 +2,9 @@ package com.example.demo.admin.audit.domain;
 
 public enum AuditAction {
 
-    USER_CREATE,
-    USER_UPDATE,
     USER_DELETE,
-
     USER_STATUS_CHANGE,
 
     ROLE_ASSIGN,
-    ROLE_REMOVE,
-
-    LOGIN,
-    LOGOUT
+    ROLE_REMOVE
 }
