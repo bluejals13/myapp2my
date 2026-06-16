4dd53eda889231da8305fca25a0c53478df5e342
bluejals13 <yoriya10113@gmail.com>
2026-06-16 23:11:54 +0900

    Update PermissionAdminController.java

--- CI-CDpart/backend/src/main/java/com/example/demo/admin/permission/PermissionAdminController.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/permission/PermissionAdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin/permissions")
+@RequestMapping("/api/admin/permissions")
 @RequiredArgsConstructor
 public class PermissionAdminController {
 
