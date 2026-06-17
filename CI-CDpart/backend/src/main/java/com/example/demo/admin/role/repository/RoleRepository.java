5d0d9082babf04c30e5e4909ff5d9e284887dd22
bluejals13 <yoriya10113@gmail.com>
2026-06-17 16:57:54 +0900

    Update RoleRepository.java

--- CI-CDpart/backend/src/main/java/com/example/demo/admin/role/repository/RoleRepository.java
+++ CI-CDpart/backend/src/main/java/com/example/demo/admin/role/repository/RoleRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.admin.role.repository;
 
 import com.example.demo.admin.role.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
