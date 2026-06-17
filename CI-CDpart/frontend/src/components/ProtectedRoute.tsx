8497c44a6460f0fb1d9a097648284f8eeb94cb2d
bluejals13 <yoriya10113@gmail.com>
2026-06-17 11:50:28 +0900

    Update ProtectedRoute.tsx

--- CI-CDpart/frontend/src/components/ProtectedRoute.tsx
+++ CI-CDpart/frontend/src/components/ProtectedRoute.tsx
@@ -2,14 +2,24 @@ import { Navigate, Outlet } from "react-router-dom";
 import FullPageSpinner from "../components/FullPageSpinner";
 import { useAuth } from "../auth/AuthContext";
 
+function ProtectedRoute({
+  permission,
+}: {
+  permission?: string;
+}) {
+
 export default function ProtectedRoute() {	// 깜박거리는 거 로딩화면으로 수정
-  const { isLoggedIn, isLoading } = useAuth();
+  const { isLoggedIn, isLoading, hasPermission } = useAuth();
   
   if (isLoading) return <FullPageSpinner />;
   
   if (!isLoggedIn) {
   return <Navigate to="/login" replace />;
   }
-
+  
+  if (permission && !hasPermission(permission)) {
+    return <Forbidden403 />;
+  }
+  
   return <Outlet />;
 }
